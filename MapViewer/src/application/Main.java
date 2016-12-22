4d036ab5832e561295cfc5fae2c0ff4625229690
HanisahRadzi <hanisah.mradzi@yahoo.com>
2016-12-22 16:57:20 +0800

    modified GUI.fxml with updated GUI design

--- MapViewer/src/application/Main.java
+++ MapViewer/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 				Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-				Scene scene = new Scene(root,600,400);
+				Scene scene = new Scene(root,800,800);
 				
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				
